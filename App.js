28d41143c4c91096bec6657ba3f80672c8ed1276
Sahbaz4321 <168401466+Sahbaz4321@users.noreply.github.com>
2025-03-29 22:55:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Weather from './Weather'
+import Navbar1 from './Navbar1'
+
+
+export default function App() {
+  return (
+    <div>
+      
+      {/* <Weather /> */}
+    
+      <Weather/>
+     
+   
+    </div>
+  )
+}
